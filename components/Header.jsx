e54ac1e30440d162d79be64cb468d5f3c310cda2
Kartik989-max <bhatnagarkartik6.com>
2024-11-25 04:05:45 +0530

    j

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,43 +1,96 @@
-import React from 'react';
-import { SignedOut, SignInButton, SignedIn } from '@clerk/nextjs'
-import Image from 'next/image';
-import Link from 'next/link';
-import { Button } from './ui/button';
-import { PenBox } from 'lucide-react';
-import UserMenu from './user-menu';
-import { checkUser } from '@/lib/checkUser';
-import UserLoader from './user-loading';
-const Header =async () => {
+// import React from 'react';
+// import { SignedOut, SignInButton, SignedIn } from '@clerk/nextjs'
+// import Image from 'next/image';
+// import Link from 'next/link';
+// import { Button } from './ui/button';
+// import { PenBox } from 'lucide-react';
+// import UserMenu from './user-menu';
+// import { checkUser } from '@/lib/checkUser';
+// import UserLoader from './user-loading';
+// const Header =async () => {
 
-  await checkUser()
+//   await checkUser()
+//   return (
+//     <header>
+//       <nav className='py-6 px-4 flex justify-between items-center'>
+//         <Link href='/'>
+//         <Image src={'/logo.png'} alt='logo' width={100} height={26} className='h w-auto object-contain'/>
+//         </Link>
+// <div className='flex items-center gap-4'>
+//     <Link href='/project/create'>
+//     <Button variant='destructive' className="flex gap-2 items-center">
+//       <PenBox size={18}/>
+//       <span>Create Project</span>
+//     </Button>
+//     </Link>
+
+//       <SignedOut>
+//         <SignInButton forceRedirectUrl='/onboarding'>
+//         <Button variant='outline'>Login</Button>
+//         </SignInButton>
+//       </SignedOut>
+
+//       <SignedIn>
+//         <UserMenu />
+//       </SignedIn>
+// </div>
+//       </nav>
+//       <UserLoader/>
+//     </header>
+//   );
+// };
+
+// export default Header;
+
+
+import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import UserMenu from "./user-menu";
+import { PenBox } from "lucide-react";
+import Image from "next/image";
+import { checkUser } from "@/lib/checkUser";
+import UserLoading from "./user-loading";
+
+async function Header() {
+  await checkUser();
+
   return (
-    <header>
-      <nav className='py-6 px-4 flex justify-between items-center'>
-        <Link href='/'>
-        <Image src={'/logo.png'} alt='logo' width={100} height={26} className='h w-auto object-contain'/>
+    <header className="container mx-auto">
+      <nav className="py-6 px-4 flex justify-between items-center">
+        <Link href="/">
+          <h1 className="text-2xl font-bold">
+            <Image
+              src={"/logo.png"}
+              alt="Zscrum Logo"
+              width={200}
+              height={56}
+              className="h-10 w-auto object-contain"
+            />
+          </h1>
         </Link>
-<div className='flex items-center gap-4'>
-    <Link href='/project/create'>
-    <Button variant='destructive' className="flex gap-2 items-center">
-      <PenBox size={18}/>
-      <span>Create Project</span>
-    </Button>
-    </Link>
-
-      <SignedOut>
-        <SignInButton forceRedirectUrl='/onboarding'>
-        <Button variant='outline'>Login</Button>
-        </SignInButton>
-      </SignedOut>
-
-      <SignedIn>
-        <UserMenu />
-      </SignedIn>
-</div>
+        <div className="flex items-center gap-4">
+          <Link href="/project/create">
+            <Button variant="destructive" className="flex items-center gap-2">
+              <PenBox size={18} />
+              <span className="hidden md:inline">Create Project</span>
+            </Button>
+          </Link>
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/onboarding">
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserMenu />
+          </SignedIn>
+        </div>
       </nav>
-      <UserLoader/>
+
+      <UserLoading />
     </header>
   );
-};
+}
 
 export default Header;
